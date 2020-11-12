7c13f311284d953716d919d3a6f93ad49ef6cff8
Aviyah :) <aviyah8@gmail.com>
2020-11-12 19:00:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,3 @@ function multiply3(){
 function divide2(){
   number /=2
 }
-add2()
-console.log(number)
-subtract5()
-console.log(number)
-multiply3()
-console.log(number)
-divide2()
-console.log(number)
-
-
