714e815cc84a6d19bd855d380bc834dc808de5fe
Aviyah :) <aviyah8@gmail.com>
2020-11-12 19:24:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function add(a, b){
 }
 
 function subtract(a, b){
-  return (a-b)
+  return(a-b)
   }
 
 function multiply(a, b){
-  return (a*b)
+  return(a*b)
 }
 
 function divide(){
-  return (a/b)
+  return(a/b)
 }
 function increment (number){
   return number +=1
